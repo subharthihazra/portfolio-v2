5478ff23c654f959b1c043118e20cff077f69eb1
subharthi75 <subharthi75@gmail.com>
2025-02-02 13:06:40 +0530

    text change

--- app/components/components/Experiences.tsx
+++ app/components/components/Experiences.tsx
@@ -11,7 +11,7 @@ type experience = {
 };
 const experiences: experience[] = [
   {
-    name: "NoScrubs Inc",
+    name: "NoScrubs Inc, Austin, USA",
     designation: "Software Development Intern ( Remote )",
     descrption:
       "Implemented OAuth for secure phone number verification, improving user authentication workflows. Integrated Stripe to enable saving and managing user payment methods, streamlining experience. Developed and enhanced core application features using Ruby on Rails for backend and Expo (React Native) for the mobile and web interface.",
@@ -20,7 +20,7 @@ const experiences: experience[] = [
     tech: ["Ruby on Rails", "React Native (expo)", "Postgresql"],
   },
   {
-    name: "Asambhav",
+    name: "Asambhav, Ahmedabad, India",
     designation: "Web Developer Intern ( Remote )",
     descrption:
       "Worked on developing a Chrome extension for generating portfolios from LinkedIn by scraping data, utilizing LLM (OpenAI) for enhanced data processing. Worked on creating another Chrome extension to generate tailored resumes for specific job postings on LinkedIn, leveraging LLM (OpenAI).",
