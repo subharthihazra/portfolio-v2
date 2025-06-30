b403a3fc09d5b1623ec823344b92c07cffa88e7a
Subharthi Hazra <subharthi75@gmail.com>
2025-07-01 01:11:32 +0530

    cv route

---
+++ app/routes/cv.tsx
@@ -0,0 +1,9 @@
+import { LoaderFunction, redirect } from "@remix-run/node";
+
+export const loader: LoaderFunction = async () => {
+  return redirect(process.env.PUBLIC_RESUME_URL!);
+};
+
+export default function CV() {
+  return null; // No need to render anything
+}
