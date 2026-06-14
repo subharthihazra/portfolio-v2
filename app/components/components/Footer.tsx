f95c11e1668c594016e85c3536abc6472f245937
Subharthi Hazra <subharthi75@gmail.com>
2026-06-15 01:26:20 +0530

    Yet another footer text change

--- app/components/components/Footer.tsx
+++ app/components/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="h-16 mx-5 flex flex-row justify-center">
       <div className="h-full">
         <div className="h-full place-content-center text-md w-fit font-dmmono text-gray-400">
-          <div>-- thanks for stopping by --</div>
+          <div>{`{ subharthi :D }`}</div>
         </div>
       </div>
     </div>
